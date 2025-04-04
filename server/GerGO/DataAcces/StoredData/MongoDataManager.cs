428dc4425bd39fe162902834598b35f57fef15e5
Assa <tamas.molnar108@gmail.com>
2025-04-04 11:12:36 +0300

    Connected to mongodb

--- server/GerGO/DataAcces/StoredData/MongoDataManager.cs
+++ server/GerGO/DataAcces/StoredData/MongoDataManager.cs
@@ -1,6 +1,25 @@
-﻿namespace GerGO.DataAcces.StoredData
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace GerGO.DataAcces.StoredData
 {
     class MongoDataManager : IStoredDataManager
     {
+        private MongoClient _client;
+        private string connectionString = "mongodb://localhost:27017";
+        public MongoDataManager()
+        {
+            _client = new MongoClient(connectionString);
+            // Connect to the 'local' database
+            var database = _client.GetDatabase("local");
+
+            // List collections in 'local' database
+            var collections = database.ListCollectionNames().ToList();
+            Console.WriteLine("Collections in 'local' database:");
+            foreach (var collection in collections)
+            {
+                Console.WriteLine(collection);
+            }
+        }
     }
 }
